2bf5b8da2955e72316d5c85ffcc4f62959768ef5
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-03 14:14:32 +0000

    Operation

--- 1_Basic/3_conversionOperation.js
+++ 1_Basic/3_conversionOperation.js
@@ -13,7 +13,7 @@ console.log(valueInNumber)
 */
 let isLoggedIn="Sahil";
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
+/* let booleanIsLoggedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 // 1=> true; 0=>false
 // ""=>false
@@ -24,3 +24,40 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+// **** Operations *********
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1= "hello"
+let str2= " Sahil"
+
+let str=str1 + str2
+console.log(str);
+
+// problem
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log(1+2+"2");
+
+console.log(+true) // true+ not possible output error
+console.log(+"") // output = 0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter =100
+gameCounter++;
+console.log("GameCounter ", gameCounter)
+
+
