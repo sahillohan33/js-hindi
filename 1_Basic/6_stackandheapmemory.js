28912c6b55eded0dd64e1d091faaaef1d818a0fb
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-05 13:51:43 +0000

    Stack and Heap Memory

---
+++ 1_Basic/6_stackandheapmemory.js
@@ -0,0 +1,23 @@
+// Stack(Primitive) , Heap(Non-Primitive) Memory
+
+let myYoutubename="Sahil"
+
+let anothername=myYoutubename
+
+anothername="chaiaurcode"
+
+console.log(myYoutubename);
+
+console.log(anothername)
+
+let userOne ={
+    email:"user@google.com",
+    upi:"user@ybi"
+}
+
+let userTwo=userOne
+
+userTwo.email="sahil@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
