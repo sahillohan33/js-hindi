6f36180b7ff105eb364e4dbe7201d4c3df2e10a4
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-06 08:09:05 +0000

    Number and Maths

--- 1_Basic/8_NumberInMath.js
+++ 1_Basic/8_NumberInMath.js
@@ -42,6 +42,6 @@ console.log(Math.floor(Math.random()*10)+1)
 const min=10
 const max=20
 
-// console.log(Math.random()*(max-min+1));
+console.log(Math.random()*(max-min+1));
 
 console.log(Math.floor(Math.random()*(max-min+1)))
