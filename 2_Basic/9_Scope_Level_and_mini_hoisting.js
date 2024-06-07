bd9f8c14f965f289a4cb31057ff4ca62e0a92813
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-07 14:52:10 +0000

    Scope Level and mini hoisting

---
+++ 2_Basic/9_Scope_Level_and_mini_hoisting.js
@@ -0,0 +1,37 @@
+
+function one(){
+    const username="Sahil"
+
+    function two(){
+        const wesbite="youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    // two()
+}
+// one()
+
+if(true){
+ const username="Sahil"
+ if(username==="Sahil"){
+    const website=" youtube"
+    console.log(username+website)
+ }
+ // console.log(website)
+}
+// console.log(username)
+
+
+// **************************** intersting example **********************************************************
+
+console.log(addOne(3))
+function addOne(num){
+    return num+1
+}
+// console.log(addOne(3))
+addTwo(7) // error
+const addTwo=function(num){
+    return num+2
+}
+// addTwo(7)
+
