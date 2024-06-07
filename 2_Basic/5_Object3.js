f20b3f0d19263b47b7ccf5722e28869ec51390b8
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-07 10:42:11 +0000

    Object Part 3

---
+++ 2_Basic/5_Object3.js
@@ -0,0 +1,12 @@
+
+const course= {
+    coursename:"Js in hindi",
+    price:"999",
+    courseInstructor:"Sahil",
+    courseMentor:"Hitesh"
+}
+// course.courseInstructor
+
+const {courseInstructor}=course
+console.log(courseInstructor)
+
