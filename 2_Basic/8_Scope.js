c556196c25bbb31e85b8d60310d1fdbd024c3f0e
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-07 14:22:32 +0000

    local and Global scope

---
+++ 2_Basic/8_Scope.js
@@ -0,0 +1,16 @@
+/* let a=6
+const b=5
+var c=4
+
+*/
+// var c=4
+let a=300
+if(true){
+    let a=5
+    const b=3
+    // var c= 2
+    console.log("Inner is  ",a)
+}
+console.log(a)
+// console.log(b)
+// console.log(c)
