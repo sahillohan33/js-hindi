a1cf07b1e71d9ba99249b82755134802379e065c
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-08 11:59:46 +0000

    switch statement

---
+++ 4_Control_flow/switchstatement.js
@@ -0,0 +1,51 @@
+/*
+switch(key){
+    case value:
+        break;
+    default:
+        break;
+}
+*/
+
+const month=3
+switch (month) {
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("Feb")
+        break;   
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break; 
+    case 5:
+        console.log("May")
+        break;
+    case 6:
+        console.log("June")
+        break;   
+    case 7:
+        console.log("July")
+        break;
+    case 8:
+        console.log("Aug")
+        break;       
+    case 9:
+        console.log("Sep")
+        break;
+    case 10:
+        console.log("Oct")
+        break;   
+    case 11:
+        console.log("Nov")
+        break;
+    case 12:
+        console.log("Dec")
+        break;       
+    default:
+        console.log("Invalid Value")
+        break;
+}
