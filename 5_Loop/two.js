b5f2dff2c23519ed673e0f0a46ea05aa57d29e4b
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-09 12:27:02 +0000

    while and do loop

---
+++ 5_Loop/two.js
@@ -0,0 +1,21 @@
+
+let index=0
+ while (index<=10) {
+    console.log(`Value of Index is ${index}`);
+    index=index+2
+ }
+
+
+ let myArray=['flash','batman','superman']
+ let myArr=0
+ while (myArr<myArray.length) {
+    console.log(`value is ${myArray[myArr]}`);
+    myArr=myArr+1
+ }
+
+let score=1
+ do {
+    
+    console.log(`Score is ${score}`);
+    score++
+ } while (score<=10);
