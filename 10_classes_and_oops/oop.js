6dd0a88302cfdb07c62a021fefe35a8d2b63d775
Sahil <92289268+sahillohan33@users.noreply.github.com>
2024-06-19 12:37:26 +0000

    class and new keyword constructor

--- 10_classes_and_oops/oop.js
+++ 10_classes_and_oops/oop.js
@@ -10,5 +10,25 @@ const user={
         console.log(this)
     }
 }
-console.log(user.username)
-console.log(user.getUserDetails())  // output: undefined
+// console.log(user.username)
+// console.log(user.getUserDetails())  // output: undefined
+// console.log(this)
+
+function User(username,loginCount,isLoggedIn){
+    this.username=username
+    this.loginCount=loginCount
+    this.isloggedIn=isLoggedIn
+    this.greeting=function(){
+        console.log(`Welcome to ${this.username}`)
+    }
+   return this
+}
+
+const userOne=new User("Sahil",12,true)
+const userTwo= new User("Aman",11,false)
+
+console.log(userOne.constructor)
+
+// console.log(userOne)
+
+console.log(userTwo)
